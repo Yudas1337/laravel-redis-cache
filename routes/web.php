1f80285835cdc531f2a6277955680af8ed468372
Yudas1337 <yudasmalabi@gmail.com>
2024-11-18 17:03:03 +0700

    Simple Redis Usage to Speedup App Performance

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Support\Benchmark;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/cached', function () {
+
+    Cache::remember('categories:all', 3600, fn() => Category::query()->get());
+    Cache::remember('products:all', 3600, fn() => Product::query()->get());
+
+    return 'OK!';
+});
+
+Route::get('/', function () {
+    Benchmark::dd([
+        'categories-all' => fn() => Category::all(),
+        'products-all' => fn() => Product::all(),
+        'categories-get' => fn() => Category::query()->get(),
+        'products-get' => fn() => Product::query()->get(),
+        'cached-categories' => fn() => Cache::get('categories:all'),
+        'cached-products' => fn() => Cache::get('products:all'),
+    ], 100);
+});
+
